ad5f6a269b5684761da6860dbca41b8d4a9473ce
John Karanja <arakanj@gmail.com>
2015-03-26 21:09:00 +0300

    showing local-variable

---
+++ hello.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+x = 5
+def method1 
+	x = 5
+	p x
+end
+
+method1
+
+p x
---
+++ localvar.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+def rectangle_area a, b
+	puts local_variables
+	return a * b
+end
+
+puts rectangle_area 5,6
